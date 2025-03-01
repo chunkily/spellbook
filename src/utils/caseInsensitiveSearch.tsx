e016347145fcef517491e96236a27df36c1bab1e
Lee Chengkai <leechengkai@gmail.com>
2025-03-01 16:05:49 +0800

    Fix case insensitive implementation

--- src/utils/caseInsensitiveSearch.tsx
+++ src/utils/caseInsensitiveSearch.tsx
@@ -2,9 +2,10 @@ export function caseInsensitiveSearch(
 	text: string,
 	inputValue: string,
 ): boolean {
-	// Replace all non-alphanumeric, non-whitespace characters with an empty string
-	const strippedInput = inputValue.replace(/[^\w\s]/g, "");
-	const regex = new RegExp(strippedInput, "i");
+	// Escape special characters in the input string
+	const escapedInput = inputValue.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+	// i flag makes the search case-insensitive
+	const regex = new RegExp(escapedInput, "i");
 
 	return regex.test(text);
 }
