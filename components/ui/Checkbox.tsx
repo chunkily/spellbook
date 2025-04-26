ab44e9703eb146a3fbfcd64ca54ca532ede01af1
Lee Chengkai <leechengkai@gmail.com>
2025-04-26 13:57:08 +0800

    Fix checks not showing

--- components/ui/Checkbox.tsx
+++ components/ui/Checkbox.tsx
@@ -42,7 +42,7 @@ export default function Checkbox({
 	return (
 		<div className={className}>
 			<input
-				className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 focus:ring-2 "
+				className="w-4 h-4 rounded-sm focus:ring-blue-500 focus:ring-2"
 				id={id}
 				type="checkbox"
 				name={name}
